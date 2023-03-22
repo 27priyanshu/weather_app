b244739e564d0d45ff0db460ba35df6f3b202cb1
Priyanshu <2003priyanshusingh@gmail.com>
2023-03-22 21:15:33 +0530

    sunny picture added

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -7,19 +7,28 @@ class WeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        // title: Text('Weather App'),
+        // title: Text(
+        //   'Weather App',
+        //   textAlign: TextAlign.center,
+        // ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         leading: IconButton(
+          onPressed: () {},
           icon: Icon(
             Icons.search,
             size: 30,
             color: Colors.white,
           ),
-          onPressed: () {},
+          // onPressed: () {},
         ),
         actions: [
           Container(
-            child: Image.asset('assets/menu-white.jpg'),
+            child: GestureDetector(
+                onTap: () => print('Menu Clicked!'),
+                child: Image.asset('assets/menu-white.jpg')),
           )
         ],
       ),
@@ -27,11 +36,26 @@ class WeatherApp extends StatelessWidget {
           child: Stack(
         children: [
           Image.asset(
-            'assets/night.jpg',
+            'assets/sunny2.jpg',
             fit: BoxFit.cover,
             height: double.infinity,
             width: double.infinity,
           ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                ),
+                Text("Delhi",
+                    style: GoogleFonts.lato(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white)),
+              ],
+            ),
+          )
         ],
       )),
     );

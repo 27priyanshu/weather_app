242c303ce4e24300d913fbe98bbb54f2a4ebb9db
Priyanshu <2003priyanshusingh@gmail.com>
2023-05-05 21:07:10 +0530

    updated

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -14,7 +14,8 @@ class WeatherApp extends StatefulWidget {
 
 class _WeatherAppState extends State<WeatherApp> {
   int _currentPage = 0;
-  late String bgImg;
+  late String bgImg ; 
+
 
   _onPageChanged(int index) {
     setState(() {
